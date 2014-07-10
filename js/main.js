6ca0379b40270a32a1308509d3eba45ba29e62d2
Mason Morris <masonmem@gmail.com>
2014-07-09 20:48:21 -0400

    Worked on fixing window height, splash page.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+/*
 $(document).ready(function(){
     var splashHeight = $(window).height() - $('#nav-affix').height();
-    $('#splash').css("height", splashHeight);
+    $('#new-splash').css("height", splashHeight);
     $('#nav-affix').affix({
         offset: {
             top: $(window).height()
         }
     });
 });
+*/
+
+jQuery.event.add(window, "load", resizeFrame);
+jQuery.event.add(window, "resize", resizeFrame);
+
+function resizeFrame() 
+{
+    var h = $(window).height();
+    $("#new-splash").css('height', h);
+    // $("#new-splash").css('height',(h < 768 || w < 1024) ? 500 : 400);
+}
