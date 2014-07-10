afd82b1b2f383e380e18dc6721b67f45abc7cd54
Chase Wang <chase@ocwang.com>
2014-07-10 19:21:45 -0400

    Disabled parallax on mobile dvices

--- js/main.js
+++ js/main.js
@@ -30,3 +30,22 @@ $(window).scroll(function() {
 
 
 document.createElement("section"); // Fix bc IE can't render section tag
+
+$(document).ready(function(){
+		var $window = $(window);
+		if( $window.width() > 768){
+			$('section[data-type="background"]').each(function(){
+		    var $bgobj = $(this); // assigning the object
+
+		    $(window).scroll(function() {
+			  	var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+
+		      // Put together our final background position
+		      var coords = '50% '+ yPos + 'px';
+
+		      // Move the background
+		      $bgobj.css({ backgroundPosition: coords });
+		    });
+		  })
+		}
+});
